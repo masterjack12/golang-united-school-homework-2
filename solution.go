f1a084a0015ef4640576bc4373e78a30e0420482
Astapova M <astap.m92@gmail.com>
2022-02-22 20:11:58 +0600

    homework2

--- solution.go
+++ solution.go
@@ -1,5 +1,10 @@
 package square
 
+import (
+	"fmt"
+	"math"
+)
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -7,6 +12,30 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+type sum int
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+const SidesTriangle = 3
+const SidesSquare = 4
+const SidesCircle = 0
+
+func CalcSquare(sideLen float64, sidesNum sum) float64 {
+
+	var square float64
+
+	if sidesNum != SidesTriangle && sidesNum != SidesSquare && sidesNum != SidesCircle {
+		square = 0
+	} else if sidesNum == SidesTriangle {
+		square = math.Pow(sideLen, float64(2)) * math.Sqrt(3) / float64(4)
+	} else if sidesNum == SidesSquare {
+		square = math.Pow(sideLen, sideLen)
+	} else if sidesNum == SidesCircle {
+		square = math.Pi * math.Pow(sideLen, sideLen)
+	}
+
+	fmt.Println(square)
+	return square
+
+}
+func main() {
+	CalcSquare(10.0, SidesTriangle)
 }
