33e3f956cab93989bca00545c2fb2c6d2bf9cfad
Astapova M <astap.m92@gmail.com>
2022-02-22 21:28:29 +0600

    fix directorys path

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"https://github.com/masterjack12/golang-united-school-homework-2/tree/master/square"
+	square "github.com/masterjack12/golang-united-school-homework-2/tree/master/square"
 )
 
 func main() {
-	CalcSquare(sideLen, sidesNum)
+	square.CalcSquare()
 
 }
